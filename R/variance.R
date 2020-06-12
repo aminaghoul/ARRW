54b8d9cbeb2c9765c0323fd8458b24c68b14723e
Amina Ghoul <amina.ghoul@outlook.com>
2020-06-12 11:01:41 +0200

    ajout simulations

--- R/variance.R
+++ R/variance.R
@@ -9,13 +9,14 @@ var <- function(val, sigma, n)
   k <- val$phip^2*val$rp^(n - 1) - val$phim^2*val$rm^(n - 1)
   k0 <- val$phip - val$phim
   u <- sqrt(1 + 4*val$om)
+  R <- log(val$rp)
 
   res <- rep(0,n)
   for(i in 1:n)
   {
-    terme1 <- 2*sinh(1)*(n + n*cosh(2*i - 1)+2*i*sinh(n - 2*i + 1)*sinh(n))
-    terme2 <- 2*sinh(n)*cosh(n - 2*i) + cosh(1)*sinh(2*n)
-    facteur <- (2*sigma^2)/(sinh(1)*(u^4)*(k*k0)^2)
+    terme1 <- 2*sinh(R)*(n + n*cosh(R*(2*i - 1))+2*i*sinh(R*(n - 2*i + 1))*sinh(R*n))
+    terme2 <- 2*sinh(R*n)*cosh(R*(n - 2*i)) + cosh(R)*sinh(2*R*n)
+    facteur <- (2*sigma^2)/(sinh(R)*(u^4)*(k*k0)^2)
 
     res[i] <- facteur*(terme1 + terme2)
   }
@@ -35,6 +36,7 @@ var <- function(val, sigma, n)
 var1 <- function(val, sigma, n)
 {
   rp <-val$rp
+  print(rp)
   omega <- val$om
   u <- sqrt(1 + 4*val$om)
 
@@ -43,7 +45,7 @@ var1 <- function(val, sigma, n)
   {
     terme1 <- 4*(n + n*(rp^(2*i - 1) + rp^(1 - 2*i))/2)/((rp-rp^(-1))^(3)*(rp^n - rp^(-n))^2)
     terme2 <- 2*(i*(rp^(n - 2*i + 1) - rp^(-n + 2*i - 1)))/((rp-rp^(-1))^(3)*(rp^n - rp^(-n)))
-    terme3 <- (rp^(2*n) - rp^(-2*n))*(rp+rp^(-1))/((rp-rp^(-1))^(4)*(rp^n - rp^(-n))^2)
+    terme3 <- (rp^(n) + rp^(-n))*(rp+rp^(-1))/((rp-rp^(-1))^(4)*(rp^n - rp^(-n)))
     terme4 <- 2*(rp^(n - 2*i) + rp^(2*i - n))/((rp-rp^(-1))^(4)*(rp^n - rp^(-n)))
     facteur <- (sigma^2)/(omega^2)
 
